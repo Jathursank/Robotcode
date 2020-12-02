942b9b3e648a073c1825aeb58ed71e01833f158f
Jathursank <71386127+Jathursank@users.noreply.github.com>
2020-12-02 14:53:27 +0100

    Spartak Jr robot
    Den blev lavet af #spartaklove kærlighed

---
+++ Spartakjr.java
@@ -0,0 +1,64 @@
+package prog;
+import robocode.*;
+//import java.awt.Color;
+
+// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
+
+/**
+ * Spartakjr - a robot by (your name here)
+ */
+public class Spartakjr extends Robot
+{
+	/**
+	 * run: Spartakjr's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			ahead(100);
+			turnRight(180);
+			turnGunRight(360);
+			ahead(100);
+			turnGunRight(180);
+			
+			
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(3);
+		
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(180);
+		ahead(100);
+		
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(60);
+		turnRight(60);
+		ahead(100);
+	}	
+}
